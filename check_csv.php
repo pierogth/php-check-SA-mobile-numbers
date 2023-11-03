292f51b8128532f7f2d5906abe23398d26783e1c
pierogth <35570213+pierogth@users.noreply.github.com>
2023-11-03 18:01:09 +0100

    Add files via upload

--- check_csv.php
+++ check_csv.php
@@ -1,18 +1,26 @@
 <?php
-
+ob_start();
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
 var_dump($_FILES);
 $tmpName = $_FILES['csv']['tmp_name'];
-$csvAsArray = fgetcsv(fopen($tmpName,"r"));
 
-print_r($csvAsArray);
 
+$outputArray=[];
+
+$file  = fopen($tmpName,"r");
+                
+$i=0;
+
+while($i<3){
+    $csvAsArray = fgetcsv($file);
+    $i++;
 foreach($csvAsArray as $line){
 
     $result="";
+    $action="";
 
     //get the nuber typed by the user
     $number = $line;
@@ -23,10 +31,12 @@ foreach($csvAsArray as $line){
    
     if($lenght==9){
         $number = "+27".$number;
+        $action="CORRECTED adding +27";
     }elseif($lenght==11){
-    echo "entrato11";
-    $number="+".$number;
     
+    $number="+".$number;
+    $action="CORRECTED adding just +";
+
     }
     
     if( preg_match( "/^\+27[0-9]{9}$/", $number ) ){
@@ -40,9 +50,44 @@ foreach($csvAsArray as $line){
       }
 
       echo "\n".$result;
+      
+      array_push($outputArray, array($number, $result, $action));
+      
+}
+}
+var_dump($outputArray);
+
+
+echo "<script>
+location='index.html';
+</script>";
+
+ob_clean();
+
+
+
+$data=[];
+// Create an array of data
+foreach($outputArray as $line)
+array_push($data, $line);
+
+//var_dump($data);
+// Open a file handle for writing
+$fp = fopen('data.csv', 'w');
+
+// Write the data to the file
+foreach ($data as $row) {
+  fputcsv($fp, $row);
 }
 
+// Close the file handle
+fclose($fp);
 
+
+// download file
+header('Content-type: text/csv');
+header('Content-disposition:attachment; filename="data.csv"');
+readfile("data.csv");
 
 
 
